67ea6b7568f96d63332f55c8d3fcfaa82c01b5e2
Joshua Kedzierski <josh.kedzierski@gmail.com>
2020-05-17 10:55:59 -0700

    adding app

--- apps/scraping.py
+++ apps/scraping.py
@@ -3,9 +3,22 @@ from splinter import Browser
 from bs4 import BeautifulSoup
 import pandas as pd
 
-# Windows users
-executable_path = {'executable_path': 'chromedriver.exe'}
-browser = Browser('chrome', **executable_path, headless=False)
+def scrape_all():
+    # Initiate headless driver for deployment
+    browser = Browser("chrome", executable_path="chromedriver", headless=True)
+    news_title, news_paragraph = mars_news(browser)
+    # Run all scraping functions and store results in dictionary
+    data = {
+        "news_title": news_title,
+        "news_paragraph": news_paragraph,
+        "featured_image": featured_image(browser),
+        "facts": mars_facts(),
+        "last_modified": dt.datetime.now()
+    }
+    browser.quit()
+    return data
+
+
 
 def mars_news(browser):
 
@@ -80,7 +93,6 @@ def mars_facts():
     # Convert dataframe into HTML format, add bootstrap
     return df.to_html()
 
-browser.quit()
 
 if __name__ == "__main__":
     # If running as script, print scraped data
